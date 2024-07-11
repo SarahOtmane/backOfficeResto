98e422ecf37b164761f6e810ce776c9a863ba580
SarahOtmane <sarahotmane02@gmail.com>
2024-07-11 17:08:31 +0200

    composant inputLabel

---
+++ src/components/InputLable.js
@@ -0,0 +1,10 @@
+
+
+export default function InputLabel(){
+    return(
+        <div>
+            <label>Nome</label>
+            <input type="text" placeholder="Digite seu nome" />
+        </div>
+    )
+}
