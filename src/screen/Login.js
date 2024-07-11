f98e332d33a79d3880929e7f971cbc654ab40457
SarahOtmane <sarahotmane02@gmail.com>
2024-07-11 17:17:32 +0200

    definir les deux inputLabel

--- src/screen/Login.js
+++ src/screen/Login.js
@@ -1,10 +1,32 @@
+import { useState } from "react";
+
+import InputLabel from "../components/InputLable";
 
 
 
 export default function Login(){
+    const [formData, setFormData] = useState({
+        email: "",
+        password: ""
+    });
+
     <main className="backBleu">
         <form>
-            
+            <InputLabel 
+                label='Email' 
+                type='email'
+                placeholder='votre e-mail'
+                value={formData.email}
+                onChange={e => setFormData({...formData, email: e.target.value})}
+            />
+
+            <InputLabel 
+                label='Password' 
+                type='password'
+                placeholder='votre mot de passe'
+                value={formData.password}
+                onChange={e => setFormData({...formData, password: e.target.value})}
+            />
         </form>
     </main>
 }
