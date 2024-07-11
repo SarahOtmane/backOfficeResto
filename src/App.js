dcfc304b9318bf2921403165c709ee9f185aa12f
SarahOtmane <sarahotmane02@gmail.com>
2024-07-11 17:01:35 +0200

    import du composant login

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import { BrowserRouter, Route, Routes} from "react-router-dom";
 
+import './App.css';
 
-
+import Login from "./screen/Login";
 
 export default function App(){
     return(
-        <></>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Login />} />
+            </Routes>
+        </BrowserRouter>
     )
 }
