7644bad6a9510ac2c98faee452602b39483ff2d6
SarahOtmane <sarahotmane02@gmail.com>
2024-07-30 13:01:54 +0200

    contenu du compoant Menu

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,10 +1,41 @@
+import resto from '../assets/icons/resto.svg';
+import planTable from '../assets/icons/planTable.svg';
+import info from '../assets/icons/info.svg';
+import reservation from '../assets/icons/reservations.svg';
+import utilisateurs from '../assets/icons/utilisateur.svg';
 
+import LienMenu from './LienMenu';
 
 
 
 export default function Menu(){
+    const navigation = [{
+        text : 'Restaurants',
+        link : '/restaurants',
+        icon : resto
+    },{
+        text : 'Plan Table',
+        link : '/planTable',
+        icon : planTable
+    },{
+        text : 'Informations',
+        link : '/informations',
+        icon : info
+    },{
+        text : 'Réservations',
+        link : '/reservations',
+        icon : reservation
+    },{
+        text : 'Utilisateurs',
+        link : '/utilisateurs',
+        icon : utilisateurs
+    }]
+
     return (
-        <>
-        </>
+        <ul>
+            {navigation.map(nav => <li>
+                <LienMenu text={nav.text} svg={nav.icon} lien={nav.link} />
+            </li>)}
+        </ul>
     )
 }
