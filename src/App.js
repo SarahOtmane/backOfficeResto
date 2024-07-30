d7f09052361911e7280c057737cfa24e8317db7c
SarahOtmane <sarahotmane02@gmail.com>
2024-07-30 13:07:59 +0200

    add restaurant components

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { BrowserRouter, Route, Routes} from "react-router-dom";
 import './App.css';
 
 import Login from "./screen/Login";
+import Menu from "./components/Menu";
 
 export default function App(){
     return(
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<Login />} />
+                <Route path="/admin" element={<Login />} />
+                <Route path="/admin/restaurants" element={<Menu />} />
             </Routes>
         </BrowserRouter>
     )
