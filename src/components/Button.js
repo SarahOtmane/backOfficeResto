d7a360affc6f99a28594ad07ac7ded870675d2fc
SarahOtmane <sarahotmane02@gmail.com>
2024-07-30 16:59:32 +0200

    update composant Button and it's button

--- src/components/Button.js
+++ src/components/Button.js
@@ -7,7 +7,7 @@ export default function Button({text, onClick, icon}){
             className="button row"
             onClick={onClick}
         >
-            {text}
+            <p>{text}</p>
             {icon && <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M12 6V18" stroke="#999999" strokeWidth="1.7" strokeLinecap="round" strokeLinejoin="round"/>
                     <path d="M6 12H18" stroke="#999999" strokeWidth="1.7" strokeLinecap="round" strokeLinejoin="round"/>
