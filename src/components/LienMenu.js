42ea8e4cb18e4779c5da2aaf54bacdff96e1e70f
SarahOtmane <sarahotmane02@gmail.com>
2024-07-30 12:56:37 +0200

    contenu d'un lien

--- src/components/LienMenu.js
+++ src/components/LienMenu.js
@@ -1,9 +1,12 @@
 
 
 
-export default function LienMenu(){
+export default function LienMenu({lien, text, svg}){
 
     return(
-        <></>
+        <a href={lien}>
+            <img src={svg} alt='icon' />
+            <span>{text}</span>
+        </a>
     )
 }
