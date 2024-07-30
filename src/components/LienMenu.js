b87b4c598f464b42f3fc4bce37b02e696e26bb11
SarahOtmane <sarahotmane02@gmail.com>
2024-07-30 13:15:34 +0200

    add new class

--- src/components/LienMenu.js
+++ src/components/LienMenu.js
@@ -4,7 +4,7 @@
 export default function LienMenu({lien, text, svg}){
 
     return(
-        <a href={lien}>
+        <a href={lien} className="lienNavigation">
             <img src={svg} alt='icon' />
             <span>{text}</span>
         </a>
