beb8c7f1a3fb8869798063e1ba54183e49886603
SarahOtmane <sarahotmane02@gmail.com>
2024-07-30 16:27:50 +0200

    update composant

--- src/screen/Restos.js
+++ src/screen/Restos.js
@@ -1,13 +1,14 @@
-
-
 import Menu from '../components/Menu';
 
 
 export default function Restos(){
 
     return(
-        <main>
+        <main className='row'>
             <Menu />
+            <div className='main backGris restos'>
+                <h1>test</h1>
+            </div>
         </main>
     )
 }
