c72f5cfe759bdfab4a032e816be05bb1f209a099
SarahOtmane <sarahotmane02@gmail.com>
2024-07-31 14:40:08 +0200

    head du composant planTable

--- src/screen/PlanTable.js
+++ src/screen/PlanTable.js
@@ -1,11 +1,24 @@
 
+import Menu from '../components/Menu';
+import Titre from '../components/Titre';
+
+import '../css/planTable.css';
 
 
 
 export default function PlanTable(){
     return(
         <main className="row">
-
+            <Menu />
+            <div className='main backGris plantable'>
+                <Titre lien='/admin/planTable' title='Plan de tables' />
+                <section className='container'>
+                    <article className='row head'>
+                        <h4>Plan de tables</h4>
+                        
+                    </article>
+                </section>
+            </div>
         </main>
     )
 }
