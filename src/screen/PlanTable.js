e7a7c2b223f165dac74b1e8f2c35f0d33e91969c
SarahOtmane <sarahotmane02@gmail.com>
2024-07-31 14:52:32 +0200

    update constantes

--- src/screen/PlanTable.js
+++ src/screen/PlanTable.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 
 import Menu from '../components/Menu';
 import Titre from '../components/Titre';
@@ -7,6 +8,46 @@ import '../css/planTable.css';
 
 
 export default function PlanTable(){
+    const [restos, setRestos] = useState([]);
+    const [planTables, setPlanTables] = useState([]);
+
+    useEffect(() => {
+        setRestos([{
+            id : 1,
+            name: 'Le Végétal',
+            nbSalles : 2,
+            nbPlaces : 50
+        }, {
+            id : 1,
+            name: 'Le Félix',
+            nbSalles : 1,
+            nbPlaces : 15
+        }]);
+
+        setPlanTables([{
+            id : 1,
+            salle: '1er étage',
+            nbTables : 10,
+            nbPlaces : 30,
+            full : false,
+            id_resto : 1
+        }, {
+            id : 2,
+            salle: 'Terrasse',
+            nbTables : 6,
+            nbPlaces : 20,
+            full : false,
+            id_resto : 1
+        }, {
+            id : 3,
+            salle: 'Salle',
+            nbTables : 6,
+            nbPlaces : 15,
+            full : false,
+            id_resto : 1
+        }]);
+    }, []);
+
     return(
         <main className="row">
             <Menu />
@@ -17,6 +58,9 @@ export default function PlanTable(){
                         <h4>Plan de tables</h4>
                         
                     </article>
+                    <div>
+
+                    </div>
                 </section>
             </div>
         </main>
