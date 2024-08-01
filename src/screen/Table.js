d3fc9868d7028b26b5fedd1e349a28d43631a8e3
SarahOtmane <sarahotmane02@gmail.com>
2024-08-01 11:59:22 +0200

    contenu du form ok

--- src/screen/Table.js
+++ src/screen/Table.js
@@ -1,3 +1,4 @@
+import Button from "../components/Button";
 import Menu from "../components/Menu";
 import SelectLabel from "../components/SelectLabel";
 import Titre from "../components/Titre";
@@ -15,10 +16,18 @@ export default function Table(){
                 <section className="container">
                     <article className="row head">
                         <h4>Tables</h4>
+                        <Button 
+                            text="Ajouter un restaurant" 
+                            icon = {true}
+                        />
                     </article>
-                    <form className="row">
+                    <form className="row tableForm">
                         <SelectLabel label='Restaurants' name='restaurants' />
                         <SelectLabel label='Salle' name='planTables' />
+                        <Button 
+                            text='Afficher'
+                            icon={false}
+                        />
                     </form>
                 </section>
             </div>
