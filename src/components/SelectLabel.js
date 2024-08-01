9913b96dc337193225ced91c20d654ecaf27a3a3
SarahOtmane <sarahotmane02@gmail.com>
2024-08-01 11:24:43 +0200

    add new class

--- src/components/SelectLabel.js
+++ src/components/SelectLabel.js
@@ -3,9 +3,9 @@ import SelectForm from "./Select";
 
 export default function SelectLabel({name, label}){
     return(
-        <>
+        <div className="column">
             <label>{label}</label>
             <SelectForm name={name} />
-        </>
+        </div>
     )
 }
