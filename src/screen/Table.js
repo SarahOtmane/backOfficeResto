2338f49b8a521edd65974b4148773b8f75b23635
SarahOtmane <sarahotmane02@gmail.com>
2024-08-01 10:45:04 +0200

    menu and head of Table screen

--- src/screen/Table.js
+++ src/screen/Table.js
@@ -1,3 +1,5 @@
+import Menu from "../components/Menu";
+import Titre from "../components/Titre";
 
 
 
@@ -6,7 +8,15 @@ export default function Table(){
     
     return(
         <main className="row"> 
-
+            <Menu />
+            <div className="main backGris table">
+                <Titre lien='/admin/tables' title='Tables' />
+                <section className="container">
+                    <article className="row head">
+                        <h4>Tables</h4>
+                    </article>
+                </section>
+            </div>
         </main>
     )
 }
